8e4e7fcfc64370283edcd367329f5e4b363ba12c
Luciano Brito <lucianocassio66@gmail.com>
2024-07-17 15:29:31 -0300

    first commit

---
+++ src/br/com/lcassio/CalculandoMedia.java
@@ -0,0 +1,14 @@
+package br.com.lcassio;
+
+public class CalculandoMedia {
+    public static void main(String args[]) {
+        double nota1 = 5.0;
+        double nota2 = 8.5;
+        double nota3 = 10.0;
+        double nota4 = 3.0;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média do aluno é: " + media);
+    }
+}
